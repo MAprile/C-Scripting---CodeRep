3c75139ad4ef0088a01795907d0fd6e82af64ee3
MAprile <50913984+MAprile@users.noreply.github.com>
2019-05-22 14:27:29 +0200

    Add files via upload

---
+++ ExampleAwarePosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleAwarePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 2.81)
+        {
+            Debug.Log("I'm about to hit te ground!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
