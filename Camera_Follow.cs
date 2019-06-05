87cdc4de4dfe0b1c9d9465cf327bf1b558bd2e40
MAprile <50913984+MAprile@users.noreply.github.com>
2019-06-05 08:07:46 +0200

    Add files via upload
    Camera that follow the player_2D

---
+++ Camera_Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player; //variabile per il personaggio o oggetto da far seguire alla telecamera
+    public float cameraDistance = 30.0f;
+
+    //Funzione per aggiustare la vista della telecamera
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    //Funzione posizionamento camera sul personaggio
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
